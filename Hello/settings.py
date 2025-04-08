4913f5029df13da2f216ca469036fbcc3854a259
Anupam Kumari <121815921+anupamkshahi@users.noreply.github.com>
2025-04-08 17:17:10 +0530

    added about.html

--- Hello/settings.py
+++ Hello/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
+from django.contrib.messages import constants as messages
 
 from pathlib import Path
 
@@ -31,13 +33,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    
 ]
 
 MIDDLEWARE = [
--- home/views.py
+++ home/views.py
@@ -1,20 +1,39 @@
 from django.shortcuts import render,HttpResponse
-
+from datetime import datetime
+from home.models import Contact 
+from django.contrib import messages
+from django.contrib import messages
+ 
 # Create your views here.
 def index(request):
     context={
         'variable1':"Annu is a nice girl",
         'variable2':"Annu is great"
     }
+    
     return render(request,'index2.html',context)
 
     #return HttpResponse("this is homepage")
 
 def about(request):
-    return HttpResponse("this is about page")
+    return render(request,'about.html')
+    #return HttpResponse("this is  about page")
 
 def services(request):
-    return HttpResponse("this is services page")
+    return render(request,'services.html')
+    #return HttpResponse("this is services page")
 
 def contact(request):
-    return HttpResponse("this is contact page")
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        desc=request.POST.get('desc')
+        
+        contact =Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+        contact.save()
+        
+    messages.success(request, "your message has been sent!")
+        
+    return render(request,'contact.html')
+    #return HttpResponse("this is contact page")
