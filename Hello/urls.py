896da9c9ed847188ab2e89fc3d840ba136c18998
Anupam Shahi <anupamkshahi@gmail.com>
2025-06-11 22:17:05 +0530

    Your commit message

--- Hello/urls.py
+++ Hello/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-admin.site.site_header = "Anu Ice Cream Admin"
-admin.site.site_title = "Anu Ice CReam Admin Portal"
-admin.site.index_title = "Welcome to Anu Ice Cream Researcher Portal"
+admin.site.site_header = "Falvor Feedback Admin"
+admin.site.site_title = "Flavor Feedback Admin Portal"
+admin.site.index_title = "Welcome to Flavor Feedback Researcher Portal"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
